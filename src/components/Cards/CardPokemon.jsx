adea9c84040a181ee22583da1913021bbfa5dca6
Jose Alpala <alpalajose@gmail.com>
2022-01-19 19:52:20 -0500

    Feat-Search: busqueda del pokemon

--- src/components/Cards/CardPokemon.jsx
+++ src/components/Cards/CardPokemon.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import { CardImg, CardTitle } from "reactstrap";
+import { CardImg, CardTitle, Spinner } from "reactstrap";
 import useFetch from "../../hooks/useFetch";
 import CardComponent from "./CardComponent";
 
 const CardPokemon = ({ url }) => {
   const dataPokemon = useFetch(url);
+  const { sprites } = dataPokemon;
 
   return (
     <CardComponent>
-      <CardTitle tag="h5">{dataPokemon.name} </CardTitle>
-      <CardImg
-        alt="Card image cap"
-        src={dataPokemon.sprites.back_default}
-        top
-        width="100%"
-      />
+      {sprites ? (
+        <>
+          <CardTitle tag="h5">{dataPokemon.name} </CardTitle>
+          <CardImg
+            alt={"Image " + dataPokemon.name}
+            src={sprites.back_default}
+            top
+            width="100%"
+          />
+        </>
+      ) : (
+        <div className="d-flex justify-content-center">
+          <Spinner size="sm">Loading...</Spinner>
+        </div>
+      )}
     </CardComponent>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,99 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // import api from "../helpers/httpcommon";
-import { Button, Container } from "reactstrap";
+import { Button, Col, Container, Input, Spinner } from "reactstrap";
 import CardPokemon from "../components/Cards/CardPokemon";
 import "../styles/Home.css";
 import useFetch from "../hooks/useFetch";
+import axios from "axios";
 
 const Home = () => {
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
   const data = useFetch(url);
+  const [pokemons, setPokemons] = useState([]);
+  const [search, Setsearch] = useState(false);
   const { results } = data;
 
+  useEffect(() => {
+    setPokemons(results);
+  }, [results]);
+
+  const [actualPage, setActualPage] = useState(20);
+  let totalPage = Math.round(1118 / 20);
+  const text = useRef(null);
+
   const nextPage = () => {
+    setActualPage(actualPage + 20);
     setUrl(data.next);
   };
 
   const previousPage = () => {
+    setActualPage(actualPage - 20);
     setUrl(data.previous);
+  };
+
+  const filter = () => {
+    let value = text.current.value;
+
+    const newData = results.filter((item) => {
+      const itemData = item.name.toUpperCase();
+      const textData = value.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+
+    if (value === "") {
+      Setsearch(false);
+    }
+
+    if (newData.length === 0) {
+      Setsearch(true);
+    }
+    setPokemons(newData);
   };
 
   return (
     <Container>
+      <div className="d-flex justify-content-end my-3">
+        <Col sm={3}>
+          <Input innerRef={text} onChange={filter} />
+        </Col>
+      </div>
       <Container className="d-flex justify-content-between">
         <Button onClick={previousPage} color="primary" outline>
           Back
         </Button>
+        <p onClick={previousPage} color="primary" outline>
+          {actualPage / 20 + "/" + totalPage}
+        </p>
         <Button onClick={nextPage} color="primary" outline>
           Next
         </Button>
       </Container>
-      {results && (
-        <div className="Card-Grid">
-          {results.map((pokemon, index) => {
-            return <CardPokemon key={index} url={pokemon.url} />;
-          })}
+      {pokemons ? (
+        search ? (
+          <div className="Card-Grid">
+            <CardPokemon
+              url={`https://pokeapi.co/api/v2/pokemon/${text.current.value}`}
+            ></CardPokemon>
+          </div>
+        ) : (
+          <div className="Card-Grid">
+            {pokemons.map((pokemon, index) => {
+              let { url } = pokemon;
+              return <CardPokemon key={index} url={url} />;
+            })}
+          </div>
+        )
+      ) : (
+        <div className="d-flex justify-content-center">
+          <Spinner color="primary" type="grow">
+            Loading...
+          </Spinner>
+          <Spinner color="secondary" type="grow">
+            Loading...
+          </Spinner>
+          <Spinner color="success" type="grow">
+            Loading...
+          </Spinner>
         </div>
       )}
     </Container>
